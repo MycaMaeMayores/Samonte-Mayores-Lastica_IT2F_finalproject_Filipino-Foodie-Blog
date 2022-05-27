22cdf6695c554e9c6e59ad0741c115a26b902e2a
MayoresMycaMae <mayoresmyca@gmail.com>
2022-05-27 23:38:09 +0800

    Services

---
+++ services.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+?>
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Filipino Foodie have develop by Samonte's Group" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180x180.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+
+
+  <title>Filipino Foodie</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./index.php" class="navbar-brand">Filipino Foodie</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./index.php" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/logo.jpg" width="55" height="55">
+            Filipino Foodie
+          </a>
+        <p>The Philippines food and culture is rich and diverse, and you will definitely love experiencing them.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./index.php" title="">Home</a></li>
+        <li><a href="./about.php" title="">About</a></li>
+        <li><a href="./services.php" title="">Services</a></li>
+        <li><a href="./contact.php" title="">Contact</a></li>
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="fa-icon" href="https://twitter.com/" title="">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </p>
+        <p>© Filipino Foodie | Website created by Samonte, Mayores, and Lastica</p>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+
+
+<div class="row">
+  <div class="col-xs-12 section-container-spacer">
+    <h1>Services</h1>
+    <p>Filipino Foodie is stunning, but so are its users. The following teams exists for the sole purpose of providing 
+      you with everything we can possibly do to help you share different foods with good quality so that it is an eye 
+      catcher to the people.</p>
+  </div>
+
+  <div class="col-xs-12 col-md-4 section-container-spacer">
+    <img class="img-responsive" alt="" src="./assets/images/satisfactory.jpg">
+    <h2>Satisfaction</h2>
+    <p>We provide happiness to the users. We help them share their culture and traditions through food so that they can 
+      be proud of their places. We bring smiles to the faces of every user by giving them the best quality of images. 
+      We improve our design so that we can satisfy and persuade the people to try the Filipino Foods.</p>
+    <a href="./contact.php" class="btn btn-primary" title=""> Get in touch</a>
+  </div>
+
+  <div class="col-xs-12 col-md-4 section-container-spacer">
+    <img class="img-responsive" alt="" src="./assets/images/wide.png">
+    <h2>Wide range Network</h2>
+    <p>We provide a wide-reaching network where the food that you share can be seen by many people across the globe. 
+      We reach different countries to let the Filipino food to be known and for others to try it. Having this wide 
+      networking means that others can see the Filipino’s culture and tradition through their food.</p>
+    <a href="./contact.php" class="btn btn-primary" title=""> Get in touch</a>
+  </div>
+
+  <div class="col-xs-12 col-md-4 section-container-spacer">
+    <img class="img-responsive" alt="" src="./assets/images/updatesecurity.jpg">
+    <h2>Update & Security</h2>
+    <p>We provide a tight security. The private information of our users can only be seen by a higher employee. 
+      Our website maintenance and security tasks have a regular basis to maintain the integrity of our website. 
+      We do updates and 24/7 incoming filtering to block potential malware, DDoS attacks, cross-site scripting, 
+      and other unwanted intrusions that could affect the website’s integrity.</p>
+    <a href="./contact.php" class="btn btn-primary" title=""> Get in touch</a>
+  </div>
+
+</div>
+
+
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+<script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
