c5de6aed1810303ec5623fb01e8de4ad67ac8b6b
MayoresMycaMae <mayoresmyca@gmail.com>
2022-05-27 23:44:57 +0800

    contact

---
+++ contact.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+?>
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Filipino Foodie have develop by Samonte's Group" name="author">
+
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180x180.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+
+
+  <title>Filipino Foodie</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./index.php" class="navbar-brand">Filipino Foodie</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./index.php" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/logo.jpg" width="55" height="55">
+            Filipino Foodie
+          </a>
+        <p>The Philippines food and culture is rich and diverse, and you will definitely love experiencing them.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./index.php" title="">Home</a></li>
+        <li><a href="./about.php" title="">About</a></li>
+        <li><a href="./services.php" title="">Services</a></li>
+        <li><a href="./contact.php" title="">Contact</a></li>
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="fa-icon" href="https://twitter.com/" title="">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </p>
+        <p>© Filipino Foodie | Website created by Samonte, Mayores, and Lastica</p>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+
+
+<div class="row">
+  <div class="col-xs-12">
+    <div class="section-container-spacer">
+      <h1>Contact</h1>
+      <p>Want to get in touch? We’d love to hear from you. Here’s how you can reach us…</p>
+    </div>
+    <div class="section-container-spacer">
+       <form action="" class="reveal-content">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="email" class="form-control" id="email" placeholder="Email">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" id="subject" placeholder="Subject">
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" rows="3" placeholder="Enter your message"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary btn-lg">Send</button>
+            </div>
+            <div class="col-md-6">
+              <ul class="list-unstyled address-container">
+                <li>
+                  <span class="fa-icon">
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                  </span>
+                  +6393 0402 9635
+                </li>
+                <li>
+                  <span class="fa-icon">
+                    <i class="fa fa-at" aria-hidden="true"></i>
+                  </span>
+                  filipinofoodie@gmail.com
+                </li>
+                <li>
+                  <span class="fa-icon">
+                    <i class="fa fa fa-map-marker" aria-hidden="true"></i>
+                  </span>
+                  Doña Aurora St. Mogpog, Marinduque
+                </li>
+              </ul>
+              <h3>Follow us on social networks</h3>
+              <a href="https://www.linkedin.com/" title="" class="fa-icon">
+                <i class="fa fa-linkedin"></i>
+              </a>
+              <a href="https://twitter.com/" title="" class="fa-icon">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+        </form>
+    </div>
+  </div>
+</div>
+
+
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+ <script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
