9189535df60fdb1e6a462808d3a40e643356daf1
MayoresMycaMae <mayoresmyca@gmail.com>
2022-05-28 00:01:26 +0800

    KareKare

---
+++ karekare.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Filipino Foodie have develop by Samonte's Group" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180x180.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+
+
+  <title>Filipino Foodie</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./index.php" class="navbar-brand">Filipino Foodie</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./index.php" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/logo.jpg" width="55" height="55">
+            Filipino Foodie
+          </a>
+        <p>The Philippines food and culture is rich and diverse, and you will definitely love experiencing them.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./index.php" title="">Home</a></li>
+        <li><a href="./about.php" title="">About</a></li>
+        <li><a href="./services.php" title="">Services</a></li>
+        <li><a href="./contact.php" title="">Contact</a></li>
+        <li><a href="./components.php" title="">Components</a></li>
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="fa-icon" href="https://dribbble.com/" title="">
+            <i class="fa fa-dribbble"></i>
+          </a>
+          <a class="fa-icon" href="https://twitter.com/" title="">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </p>
+        <p>© Filipino Foodie | Website created by Samonte, Mayores, and Lastica</p>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+
+
+<div class="row">
+  <div class="col-xs-12 col-md-8">
+
+    <div class="section-container-spacer">
+        <h1>Kare - kare</h1>
+        <p>Kare-kare is a Philippine stew (kare derives from "curry") that features a thick 
+            savory peanut sauce. It is generally made from a base of stewed oxtail, beef tripe, 
+            pork hocks, calves feet, pig's feet or trotters, various cuts of pork, beef stew meat, 
+            and occasionally offal. Vegetables, such as eggplant, Chinese cabbage, or other greens, 
+            daikon, green beans, okra, and asparagus beans, are added. </p>
+    </div>
+
+    <div class="section-container-spacer">
+      <p><img class="img-responsive" alt="" src="./assets/images/kk1.jpg"></p>
+      <div class="row">
+          <div class="col-xs-12 col-md-6">
+              <p><img class="img-responsive" alt="" src="./assets/images/kk2.jpg"></p>
+          </div>
+          <div class="col-xs-12 col-md-6">
+              <p><img class="img-responsive" alt="" src="./assets/images/kk3.jpg"></p>
+          </div>
+      </div>
+    </div>
+
+    <p>  The stew is flavored with ground roasted peanuts or peanut butter, onions, and garlic. It is 
+        colored with annatto and can be thickened with toasted or plain ground rice.[1][2] Variations 
+        of kare-kare can be made with seafood, such as (prawns, squid, and mussels, or exclusively from 
+        vegetables. Condiments and other flavorings are usually added. It is often eaten with bagoong 
+        (shrimp paste), sometimes spiced with chili, ginisáng bagoóng (spiced and sautéed shrimp paste), 
+        and sprinkled with calamansi juice. Other seasonings are added at the table. Variants may include 
+        goat meat or (rarely) chicken.</p>
+  </div>
+  
+</div>
+    
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+<script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
