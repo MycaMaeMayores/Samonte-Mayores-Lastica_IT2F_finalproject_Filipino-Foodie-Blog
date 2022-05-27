08dda107b7fc3836dfd100f7696db7a779f18d18
MayoresMycaMae <mayoresmyca@gmail.com>
2022-05-27 23:34:59 +0800

    About

---
+++ about.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+?>
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Filipino Foodie have develop by Samonte's Group" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180x180.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+
+
+  <title>Filipino Foodie</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./index.php" class="navbar-brand">Filipino Foodie</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./index.php" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/logo.jpg" width="55" height="55">
+            Filipino Foodie
+          </a>
+        <p>The Philippines food and culture is rich and diverse, and you will definitely love experiencing them.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./index.php" title="">Home</a></li>
+        <li><a href="./about.php" title="">About</a></li>
+        <li><a href="./services.php" title="">Services</a></li>
+        <li><a href="./contact.php" title="">Contact</a></li>
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="fa-icon" href="https://twitter.com/" title="">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </p>
+        <p>© Filipino Foodie | Website created by Samonte, Mayores, and Lastica</p>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+
+
+<div class="row">
+  <div class="col-xs-12 col-md-6">
+    <img class="img-responsive" alt="" src="./assets/images/abt.png">
+  </div>
+  <div class="col-xs-12 col-md-6">
+    <h1>About Us</h1>
+    
+    <p>We are Filipino Foodie.</p> 
+    
+    <p>Providing you with the most delicious and amazing cuisine that Filipino’s love.</p>
+    
+    <p>At Filipino Foodie, we want to share the uniqueness and love of the Filipino’s through their cuisine. 
+      People need to know that Filipino cuisine is different and has their own uniqueness based on their places, 
+      culture and traditions Filipino cuisine is popular for its delicious taste and appetizing aroma.</p>
+
+    <p>We created Filipino Foodie so that it can reach other provinces in the Philippines and hopefully can share 
+      their foods. We believe that there are more amazing foods out there hidden in other parts of the Philippines. 
+      Our mission is to let the world know that Filipino Foods are the best and worth to try.</p>
+      
+    <p>At Filipino Foodie, we don't just share foods. We make people happy and feel loved. Filipino Foodie was built 
+       on the belief that every person and place is special, and we carry that belief into everything we do.</p>
+
+    
+    
+    <a href="contact.php" class="btn btn-primary" title=""> Get in touch</a>
+ </div>
+</div>
+    
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID 
+
+<script>
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-XXXXX-X', 'auto');
+  ga('send', 'pageview');
+</script>
+
+--> <script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
