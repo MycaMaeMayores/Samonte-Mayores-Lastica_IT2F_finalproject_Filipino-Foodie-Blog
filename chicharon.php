0cb2f14093f718755ffeb2d36dbd3a053d444b03
MayoresMycaMae <mayoresmyca@gmail.com>
2022-05-27 23:57:45 +0800

    Chicharon

---
+++ chicharon.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Filipino Foodie have develop by Samonte's Group" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180x180.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+
+
+  <title>Filipino Foodie</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./index.php" class="navbar-brand">Filipino Foodie</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./index.php" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/logo.jpg" width="55" height="55">
+            Filipino Foodie
+          </a>
+        <p>The Philippines food and culture is rich and diverse, and you will definitely love experiencing them.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./index.php" title="">Home</a></li>
+        <li><a href="./about.php" title="">About</a></li>
+        <li><a href="./services.php" title="">Services</a></li>
+        <li><a href="./contact.php" title="">Contact</a></li>
+        <li><a href="./components.php" title="">Components</a></li>
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="fa-icon" href="https://dribbble.com/" title="">
+            <i class="fa fa-dribbble"></i>
+          </a>
+          <a class="fa-icon" href="https://twitter.com/" title="">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </p>
+        <p>© Filipino Foodie | Website created by Samonte, Mayores, and Lastica</p>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+
+
+<div class="row">
+  <div class="col-xs-12 col-md-8">
+
+    <div class="section-container-spacer">
+        <h1>Chicharrón</h1>
+        <p>Chicharon, less commonly spelled tsitsaron, is ubiquitous, as it is a well-loved snack, 
+            and may be bought anywhere, from large supermarket chains to small neighborhood sari-sari 
+            stores and street vendors. It is popular as pulutan or tapas food to be eaten while 
+            consuming alcoholic beverages. It is also used as a topping on many native vegetable 
+            and noodle dishes. Pork chicharon is prepared by deep-frying dried pork rind with a 
+            little salt. It may be dipped in coconut vinegar spiced with soy sauce, chopped garlic 
+            and labuyo chili peppers, or eaten with other condiments like bagoong anchovies, lechon 
+            gravy sauce, or atchara papaya salad.</p>
+    </div>
+
+    <div class="section-container-spacer">
+      <p><img class="img-responsive" alt="" src="./assets/images/chi1.jpg"></p>
+      <div class="row">
+          <div class="col-xs-12 col-md-6">
+              <p><img class="img-responsive" alt="" src="./assets/images/chi2.jpg"></p>
+          </div>
+          <div class="col-xs-12 col-md-6">
+              <p><img class="img-responsive" alt="" src="./assets/images/chi3.jpg"></p>
+          </div>
+      </div>
+    </div>
+
+    <p> Aside from pork rind, chicken skin may also be made into chicharong manok, whereas chicharong 
+        bulaklak—literally 'flower chicharrón', named for its distinctive shape—is made from pig offal. 
+        Tuna-skin chicharon is marketed as a healthier variation. In Cagayan, water buffalo hide is used 
+        to make "carabao chicharon". A distinct variant of chicharon involves cooking pork loin and meat 
+        in the same way, becoming bagnet. While similar and dissimilar to crispy pata, it is more popular 
+        as a meal than finger food, and has found its way to contemporary dishes and restaurants. Bagnet 
+        originates from Ilocos where it has been served since the 1960s.</p>
+  </div>
+  
+</div>
+    
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+<script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
