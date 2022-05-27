8153bf53932fca09f7daad52eaa1d39b57e82edc
MayoresMycaMae <mayoresmyca@gmail.com>
2022-05-27 23:27:54 +0800

    Index

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+?>
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="Page description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Filipino Foodie have develop by Samonte's Group" name="author">
+
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/logo.jpg" rel="apple-touch-icon">
+ 
+
+  <title>Filipino Foodie</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./index.php " class="navbar-brand">Filipino Foodie</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./index.php" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/logo.jpg" width="55" height="55">
+            Filipino Foodie
+          </a>
+        <p>The Philippines food and culture is rich and diverse, and you will definitely love experiencing them.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./index.php" title="">Home</a></li>
+        <li><a href="./about.php" title="">About</a></li>
+        <li><a href="./services.php" title="">Services</a></li>
+        <li><a href="./contact.php" title="">Contact</a></li>
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="fa-icon" href="https://twitter.com/" title="">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </p>
+        <p>© Filipino Foodie | Website created by Samonte, Mayores, and Lastica</p>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+ 
+<div class="hero-full-wrapper">
+  <div class="grid">
+  <div class="gutter-sizer"></div>
+    <div class="grid-sizer"></div>
+    
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/ac.jpg">
+      <a href="./project.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Arroz Caldo</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/adobo.jpg">
+      <a href="./adobo.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Adobo</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/ss.jpg">
+      <a href="./sisig.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Sisig</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/lumpia.jpg">
+      <a href="./lumpia.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Lumpia</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/pans.jpg">
+      <a href="./pansit.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Pansit</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/sinigang.jpg">
+      <a href="./sinigang.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Sinigang</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/lechon.jpg">
+      <a href="./lechon.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Lechon</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/cassava.jpg">
+      <a href="./cassava.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Cassava Cake</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/karekare.jpg">
+      <a href="./karekare.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Kare - Kare</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/dinuguan.jpg">
+      <a href="./dinuguan.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Dinuguan with Puto</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/bicol.jpg">
+      <a href="./bicol.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Bicol Express</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/halo.jpg">
+      <a href="./halohalo.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Halo halo</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+     <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/taho.jpg">
+      <a href="./taho.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Taho</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/suman.jpg">
+      <a href="./suman.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Suman</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+  </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/chicharon.jpg">
+      <a href="./chicharon.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Chicharrón</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+      </a>
+       </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/balut.jpg">
+      <a href="./balut.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Balut</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+        </a>
+       </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/torta.jpg">
+      <a href="./torta.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Torta</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+        </a>
+       </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/leche3.jpg">
+      <a href="./leche.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Leche Flan</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+        </a>
+       </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/batchoy.jpg">
+      <a href="./batchoy.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Batchoy</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+        </a>
+       </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/sapin.jpg">
+      <a href="./sapinsapin.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Sapin - sapin</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+        </a>
+       </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/liempo.jpg">
+      <a href="./liempo.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Liempo</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+        </a>
+       </div>
+      <div class="grid-item">
+      <img class="img-responsive" alt="" src="./assets/images/buko.jpg">
+      <a href="./bukopandan.php" class="project-description">
+        <div class="project-text-holder">
+          <div class="project-text-inner">
+            <h3>Buko Pandan</h3>
+            <p>Discover</p>
+          </div>
+        </div>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function (event) {
+     masonryBuild();
+  });
+</script>
+
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+<script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
