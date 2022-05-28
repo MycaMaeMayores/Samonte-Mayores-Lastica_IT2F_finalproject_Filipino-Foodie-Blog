975c7ec5ba99706cb35edcf42a62d4cd9acf588d
MayoresMycaMae <mayoresmyca@gmail.com>
2022-05-28 11:05:11 +0800

    SapinSapin

---
+++ sapinsapin.php
@@ -0,0 +1,122 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Filipino Foodie have develop by Samonte's Group" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-icon-180x180.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+
+
+  <title>Filipino Foodie</title>  
+
+<link href="./main.82cfd66e.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+<header class="">
+  <div class="navbar navbar-default visible-xs">
+    <button type="button" class="navbar-toggle collapsed">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="./index.php" class="navbar-brand">Filipino Foodie</a>
+  </div>
+
+  <nav class="sidebar">
+    <div class="navbar-collapse" id="navbar-collapse">
+      <div class="site-header hidden-xs">
+          <a class="site-brand" href="./index.php" title="">
+            <img class="img-responsive site-logo" alt="" src="./assets/logo.jpg" width="55" height="55">
+            Filipino Foodie
+          </a>
+        <p>The Philippines food and culture is rich and diverse, and you will definitely love experiencing them.</p>
+      </div>
+      <ul class="nav">
+        <li><a href="./index.php" title="">Home</a></li>
+        <li><a href="./about.php" title="">About</a></li>
+        <li><a href="./services.php" title="">Services</a></li>
+        <li><a href="./contact.php" title="">Contact</a></li>
+        <li><a href="./components.php" title="">Components</a></li>
+
+      </ul>
+
+      <nav class="nav-footer">
+        <p class="nav-footer-social-buttons">
+          <a class="fa-icon" href="https://www.instagram.com/" title="">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a class="fa-icon" href="https://dribbble.com/" title="">
+            <i class="fa fa-dribbble"></i>
+          </a>
+          <a class="fa-icon" href="https://twitter.com/" title="">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </p>
+        <p>© Filipino Foodie | Website created by Samonte, Mayores, and Lastica</p>
+      </nav>  
+    </div> 
+  </nav>
+</header>
+<main class="" id="main-collapse">
+
+
+<div class="row">
+  <div class="col-xs-12 col-md-8">
+
+    <div class="section-container-spacer">
+        <h1>Sapin - sapin</h1>
+        <p>Made with galapong (glutinous rice dough), coconut milk, sugar, condensed milk, and 
+            an occasional ube or jackfruit flavoring, this festive and colorful dish has a dense, 
+            chewy pudding-like texture, and is often seen at social gatherings or festive occasions. 
+            This colorful delicacy is very time-consuming to make.</p>
+    </div>
+
+    <div class="section-container-spacer">
+      <p><img class="img-responsive" alt="" src="./assets/images/sapin1.jpg"></p>
+      <div class="row">
+          <div class="col-xs-12 col-md-6">
+              <p><img class="img-responsive" alt="" src="./assets/images/sapin2.jpg"></p>
+          </div>
+          <div class="col-xs-12 col-md-6">
+              <p><img class="img-responsive" alt="" src="./assets/images/sapin3.jpg"></p>
+          </div>
+      </div>
+    </div>
+
+    <p> The process starts with making the sticky rice dough from scratch, then extracting the 
+        coconut milk and cooking ube halaya which is one of its many layers. It usually takes a 
+        whole day to finish this dessert. Sapin-Sapin  is a festive looking dessert with vibrant 
+        colors and is usually served with latik or toasted coconut curd. It is visually appealing 
+        and typically has 3 layers; purple, yellow, and white. </p>
+  </div>
+  
+</div>
+    
+</main>
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+  navActivePage();
+});
+</script>
+
+<script type="text/javascript" src="./main.85741bff.js"></script></body>
+
+</html>
